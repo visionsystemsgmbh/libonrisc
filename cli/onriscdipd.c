8bdf07700be1f94c14feaaf36040432248363dff
Falco Hyfing <falco.hyfing@vscom.de>
2016-08-02 13:03:07 +0000

    onriscdipd: add gpio handling (-k & -g)

--- cli/onriscdipd.c
+++ cli/onriscdipd.c
@@ -60,6 +60,25 @@ dip_entry dip_list[16] = {
 	{.action_type=UNSET}, //15
 };
 
+dip_entry gpio_list[16] = { 
+	{.action_type=UNSET}, // IN 0 RISING EDGE
+	{.action_type=UNSET}, // IN 1
+	{.action_type=UNSET}, // IN 2
+	{.action_type=UNSET}, // IN 3
+	{.action_type=UNSET}, // IN 0 FALLING EDGE
+	{.action_type=UNSET}, // IN 1
+	{.action_type=UNSET}, // IN 2
+	{.action_type=UNSET}, // IN 3
+	{.action_type=UNSET}, // 
+	{.action_type=UNSET}, //
+	{.action_type=UNSET}, //
+	{.action_type=UNSET}, //
+	{.action_type=UNSET}, //
+	{.action_type=UNSET}, //
+	{.action_type=UNSET}, //
+	{.action_type=UNSET}, //
+};
+
 bool rs232only = false;
 bool hw_uart_dips = false;
 
@@ -84,6 +103,34 @@ int test_callback(onrisc_gpios_t dips, void* data)
  
 }	
 
+int gpio_callback(onrisc_gpios_t gpios, void* data)
+{
+	int port_nr = ALL_PORTS;
+	int i;
+	char cmd[256];
+	
+	if(gpios.mask == 0) {
+		port_nr = *((int *) data);
+	}
+	for(i=0 ; i < 4 ; i++) {
+		if (gpios.mask & (1 << i)) {
+			if (gpios.value & (1 << i)) {
+				if(gpio_list[i].action_type == SERIAL_MODE) {
+					onrisc_set_uart_mode_raw(port_nr, gpio_list[i].action.mode);
+				} else 	if(gpio_list[i].action_type == SHELL) {
+					system(gpio_list[i].action.cmd);
+				}
+			} else {
+				if(gpio_list[i+4].action_type == SERIAL_MODE) {
+					onrisc_set_uart_mode_raw(port_nr, gpio_list[i+4].action.mode);
+				} else 	if(gpio_list[i+4].action_type == SHELL) {
+					system(gpio_list[i+4].action.cmd);
+				}
+			}
+		}
+	}
+}	
+
 void print_usage()
 {
 	fprintf(stderr, "\nOnRISC DIP Daemon (Version %s)\n\n", LIBONRISC_VERSION);
@@ -92,7 +139,11 @@ void print_usage()
 	fprintf(stderr,
 		"Options: -c                 read configs from /etc/onriscdipd.conf\n");
 	fprintf(stderr,
-		"         -d                 start as daemon\n");
+		"         -k                 read configs from /etc/onriscgpiod.conf\n");
+	fprintf(stderr,
+		"         -d                 start as DIP daemon\n");
+	fprintf(stderr,
+		"         -g                 start as GPIO daemon\n");
 	fprintf(stderr, "         -e                 execute callback at start\n");
 }
 
@@ -157,6 +208,67 @@ int readconfig(char *filename)
 	return 0;
 }
 
+int readconfig_gpio(char *filename)
+{
+	FILE *fp;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	int idx,i;
+	uint8_t mode = 0;
+	char type[20];
+	char cmd[256];
+	char *buf = NULL;
+
+	fp = fopen("/etc/onriscgpiod.conf", "r");
+	if (fp == NULL)
+		exit(EXIT_FAILURE);
+
+	while ((read = getline(&line, &len, fp)) != -1) {
+		if (!read)
+			continue;
+		buf=strtok(line,":");
+		sscanf(buf, "%01X", &idx);
+		buf=strtok(NULL,":");
+		sscanf(buf, "%s", type);
+		if (!strcmp(type,"unset")){
+			gpio_list[idx].action_type = UNSET;
+		} else if (!strcmp(type, "serial-mode")){
+			if (rs232only || hw_uart_dips){
+				gpio_list[idx].action_type = UNSET;
+				continue;
+			}
+			gpio_list[idx].action_type = SERIAL_MODE;
+			buf=strtok(NULL,"\n");
+			sscanf(buf, "%s", cmd);
+
+			if(strlen(cmd) > 5)
+				exit(EXIT_FAILURE);
+
+			mode = 0;
+			for (i=0; i<strlen(cmd); ++i){
+				if (cmd[i] == '1') {
+					mode |= 1 << ((strlen(cmd) - 1) - i);
+				} else if (cmd[i] == '0') {
+				} else {
+					exit(EXIT_FAILURE);
+				}	
+			}
+			
+			gpio_list[idx].action.mode = mode;
+		} else if (!strcmp(type, "shell")){
+			gpio_list[idx].action_type = SHELL;
+
+			if(strlen(cmd) > 255)
+				exit(EXIT_FAILURE);
+
+			buf=strtok(NULL,"\n");
+			strcpy(gpio_list[idx].action.cmd, buf);
+		}
+	}
+	return 0;
+}
+
 int main(int argc, char **argv)
 {
 	int opt, i;
@@ -193,7 +305,7 @@ int main(int argc, char **argv)
 	hw_uart_dips = !!(caps->uarts->flags & UARTS_DIPS_PHYSICAL);	
 	
 	/* handle command line params */
-	while ((opt = getopt(argc, argv, "cedh?")) != -1) {
+	while ((opt = getopt(argc, argv, "ckedgh?")) != -1) {
 		switch (opt) {
 
 		case 'c':
@@ -227,6 +339,19 @@ int main(int argc, char **argv)
 				onrisc_get_dips(&dips.value);
 				test_callback(dips,&i);
 			}
+			break;
+		case 'g':
+			if (!caps->gpios) {
+				printf("This device has GPIOs\nNothing to do for daemon\n");
+				break;
+			}
+			onrisc_gpios_t mask;
+			mask.mask=0xF;
+			onrisc_gpio_register_callback(mask, gpio_callback, NULL, BOTH);
+			while(1);
+			break;
+		case 'k':
+			readconfig_gpio(NULL);
 			break;
 		case '?':
 		case 'h':
