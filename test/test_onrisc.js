9c9f7e0bb4dced8aa5628b07da8f8e0f4c9020df
Vision Systems GmbH <contact@visionsystems.de>
2017-10-09 09:25:38 +0200

    Add the same tests for both Python and Node.js

--- test/test_onrisc.js
+++ test/test_onrisc.js
@@ -4,4 +4,76 @@ var info = new onrisc.onrisc_system_t();
 onrisc.onrisc_init(info);
 
 console.log("Serial number: %d", info.ser_nr);
-console.log("Production date %s", info.prd_date);
+console.log("Production date: %s", info.prd_date);
+
+var caps = onrisc.onrisc_get_dev_caps();
+
+// GPIO
+if (caps.gpios) {
+	var gpios = new onrisc.onrisc_gpios_t();
+	onrisc.onrisc_gpio_get_value(gpios);
+	console.log("Show GPIOs value: 0x%s", gpios.value.toString(16));
+}
+
+// DIP
+if (caps.dips) {
+	dip = onrisc.new_uint32_tp();
+	onrisc.onrisc_get_dips(dip);
+	val = onrisc.uint32_tp_value(dip);
+	if (val & onrisc.DIP_S1) {
+		console.log("S1: on");
+	} else {
+		console.log("S1: off");
+	}
+	if (val & onrisc.DIP_S2) {
+		console.log("S2: on");
+	} else {
+		console.log("S2: off");
+	}
+	if (val & onrisc.DIP_S3) {
+		console.log("S3: on");
+	} else {
+		console.log("S3: off");
+	}
+	if (val & onrisc.DIP_S4) {
+		console.log("S4: on");
+	} else {
+		console.log("S4: off");
+	}
+	onrisc.delete_uint32_tp(dip);
+}
+
+// WLAN switch
+if (caps.wlan_sw) {
+	wlan_sw  = onrisc.new_intp();
+	onrisc.onrisc_get_wlan_sw_state(wlan_sw);
+	val = onrisc.intp_value(wlan_sw);
+	if (val) {
+		console.log("WLAN switch: on");
+	} else {
+		console.log("WLAN switch: off");
+	}
+	onrisc.delete_intp(wlan_sw);
+}
+
+// mPCIe switch
+if (caps.mpcie_sw) {
+	mpcie_sw  = onrisc.new_intp();
+	onrisc.onrisc_get_mpcie_sw_state(mpcie_sw);
+	val = onrisc.intp_value(mpcie_sw);
+	if (val) {
+		console.log("mPCIe switch: on");
+	} else {
+		console.log("mPCIe switch: off");
+	}
+	onrisc.delete_intp(mpcie_sw);
+
+	onrisc.onrisc_set_mpcie_sw_state(0);
+}
+
+// UARTs
+if (caps.uarts) {
+	var uart = new onrisc.onrisc_uart_mode_t();
+	uart.rs_mode = onrisc.TYPE_RS485_HD;
+	onrisc.onrisc_set_uart_mode(1, uart);
+}
--- test/test_onrisc.py
+++ test/test_onrisc.py
@@ -1,7 +1,49 @@
+#!/usr/bin/python
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+
 from onrisc import *
 
-sys = onrisc_system_t()
+info = onrisc_system_t()
 
-onrisc_init(sys)
+onrisc_init(info)
+
+print("Serial number: {}".format(info.ser_nr))
+print("Production date: {}".format(info.prd_date))
 
 caps = onrisc_get_dev_caps()
+
+# GPIO
+if caps.gpios:
+    gpios = onrisc_gpios_t()
+    onrisc_gpio_get_value(gpios)
+    print("Show GPIOs value: 0x{0:08x}".format(gpios.value))
+
+# DIP
+if caps.dips:
+    rc, dips = onrisc_get_dips()
+    print("Show DIPs value: 0x{0:08x}".format(dips))
+
+# WLAN switch
+if caps.wlan_sw:
+    rc, wlan_sw = onrisc_get_wlan_sw_state()
+    if wlan_sw:
+        print("WLAN switch: on")
+    else:
+        print("WLAN switch: off")
+
+# mPCIe switch
+if caps.mpcie_sw:
+    rc, mpcie_sw = onrisc_get_mpcie_sw_state()
+    if mpcie_sw:
+        print("mPCIe switch: on")
+    else:
+        print("mPCIe switch: off")
+
+    onrisc_set_mpcie_sw_state(0)
+
+# UARTs
+if caps.uarts:
+    uart = onrisc_uart_mode_t()
+    uart.rs_mode = TYPE_RS485_HD
+    onrisc_set_uart_mode(1, uart)
