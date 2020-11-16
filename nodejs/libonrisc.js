31de7ec444202b1f6510b52639a2a90924d839a3
Vision Systems GmbH <contact@visionsystems.de>
2020-11-16 12:37:27 +0100

    Nodejs: add LED support

--- nodejs/libonrisc.js
+++ nodejs/libonrisc.js
@@ -14,6 +14,12 @@ const rs_type = {
 	TYPE_CAN: 7,
 }
 
+const led_type = {
+	LED_POWER: 1,
+	LED_WLAN: 2,
+	LED_APP: 4
+}
+
 var onrisc_system_t = Struct({
   'model' : 'uint16',
   'hw_rev' : 'uint32',
@@ -34,6 +40,28 @@ var onrisc_uart_mode_t = Struct({
 });
 var onrisc_uart_mode_ptr = ref.refType(onrisc_uart_mode_t);
 
+// define the time types
+var time_t = ref.types.long
+var suseconds_t = ref.types.long
+
+// define the "timeval" struct type
+var timeval = Struct({
+  tv_sec: time_t,
+  tv_usec: suseconds_t
+})
+
+var blink_led_t = Struct({
+  'interval' : timeval,
+  'high_phase' : timeval,
+  'led_type' : 'uint32',
+  'count' : 'int32',
+  'thread_id' : 'uint32',
+  'led' : ref.refType('void'),
+  'fd' : 'int',
+  'leds_old' : 'uint32',
+});
+var blink_led_ptr = ref.refType(blink_led_t);
+
 var lib_file = 'libonrisc.so';
 
 // loads all symbols from the shared object for the libonrisc.so
@@ -48,11 +76,15 @@ var onrisc = ffi.Library(lib_file, {
   'onrisc_get_mpcie_sw_state': ['int32', ['int32*']],
   'onrisc_set_mpcie_sw_state': ['int32', ['int32']],
   'onrisc_get_dips': ['int32', ['uint32*']],
+  'onrisc_blink_create': ['void', [blink_led_ptr]],
+  'onrisc_switch_led': ['int32', [blink_led_ptr, 'uint8']],
 });
 
 exports.rs_type = rs_type
+exports.led_type = led_type
 exports.onrisc_system_t = onrisc_system_t
 exports.onrisc_uart_mode_t = onrisc_uart_mode_t
+exports.blink_led_t = blink_led_t
 exports.onrisc_init = onrisc.onrisc_init
 exports.onrisc_get_uart_mode = onrisc.onrisc_get_uart_mode
 exports.onrisc_set_uart_mode = onrisc.onrisc_set_uart_mode
@@ -60,3 +92,5 @@ exports.onrisc_get_wlan_sw_state = onrisc.onrisc_get_wlan_sw_state
 exports.onrisc_get_mpcie_sw_state = onrisc.onrisc_get_mpcie_sw_state
 exports.onrisc_set_mpcie_sw_state = onrisc.onrisc_set_mpcie_sw_state
 exports.onrisc_get_dips = onrisc.onrisc_get_dips
+exports.onrisc_blink_create = onrisc.onrisc_blink_create
+exports.onrisc_switch_led = onrisc.onrisc_switch_led
